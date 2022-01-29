1104e26f0ad1ee2547691afb8a7127669d83ee3a
Paul McKellar <git@paulmckellar.com>
2022-01-29 09:08:01 -0500

    deploy gets snapshot

--- polygon-contracts/scripts/deploy.js
+++ polygon-contracts/scripts/deploy.js
@@ -1,23 +1,31 @@
 const hre = require('hardhat');
+const fs = require('fs');
 
 async function main() {
-  const Pack = await hre.ethers.getContractFactory('Pack');
-  const pack = await Pack.deploy();
-  await pack.deployed();
+  // const Pack = await hre.ethers.getContractFactory('Pack');
+  // const pack = await Pack.deploy();
+  // await pack.deployed();
 
-  const AdventureCards = await hre.ethers.getContractFactory('AdventureCards');
-  const adventureCards = await AdventureCards.deploy();
-  await adventureCards.deployed();
+  // const AdventureCards = await hre.ethers.getContractFactory('AdventureCards');
+  // const adventureCards = await AdventureCards.deploy();
+  // await adventureCards.deployed();
 
-  const Snapshot = await hre.ethers.getContractFactory('Snapshot');
-  const shapshot = await Snapshot.deploy();
-  await shapshot.deployed();
+  // const Snapshot = await hre.ethers.getContractFactory('Snapshot');
+  // const shapshot = await Snapshot.deploy();
+  // await shapshot.deployed();
 
-  await pack.setAdventureCards(adventureCards.address); // where individual cards are stored
-  await pack.setSnapshot(shapshot.address, true); // so only snapshot can update the pack
-  await shapshot.setPack(pack.address); // needs to mint
+  // await pack.setAdventureCards(adventureCards.address); // where individual cards are stored
+  // await pack.setSnapshot(shapshot.address, true); // so only snapshot can update the pack
+  // await shapshot.setPack(pack.address); // needs to mint
 
   console.log('Deployed:', pack.address);
+
+  try {
+    const data = fs.readFileSync('../snapshot/snapshot.csv', 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 main().catch((error) => {
