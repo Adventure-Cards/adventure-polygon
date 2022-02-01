e563936238f4af1078135efa239ffe845908ae1a
Paul McKellar <git@paulmckellar.com>
2022-02-01 09:12:11 -0500

    fix addressses

--- polygon-contracts/scripts/deploy.js
+++ polygon-contracts/scripts/deploy.js
@@ -5,21 +5,21 @@ async function main() {
   const Pack = await hre.ethers.getContractFactory('Pack');
   const pack = await Pack.deploy();
   await pack.deployed();
+  console.log('Deployed pack:', pack.address);
 
   const AdventureCards = await hre.ethers.getContractFactory('AdventureCards');
   const adventureCards = await AdventureCards.deploy();
   await adventureCards.deployed();
-  console.log('Deployed card:', pack.address);
+  console.log('Deployed card:', adventureCards.address);
 
   const Snapshot = await hre.ethers.getContractFactory('Snapshot');
   const snapshot = await Snapshot.deploy();
   await snapshot.deployed();
-  console.log('Deployed snapshot:', pack.address);
+  console.log('Deployed snapshot:', snapshot.address);
 
   await pack.setAdventureCards(adventureCards.address); // where individual cards are stored
   await pack.setSnapshot(snapshot.address, true); // so only snapshot can update the pack
   await snapshot.setPack(pack.address); // needs to mint
-  console.log('Deployed pack:', pack.address);
 }
 
 main().catch((error) => {
