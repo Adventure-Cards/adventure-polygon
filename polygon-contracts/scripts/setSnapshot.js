5a3a2118f4ae1ecf66d909836d0b681fef72dc98
Paul McKellar <git@paulmckellar.com>
2022-02-01 09:09:47 -0500

    set owners 3 at a time

--- polygon-contracts/scripts/setSnapshot.js
+++ polygon-contracts/scripts/setSnapshot.js
@@ -4,15 +4,21 @@ const fs = require('fs');
 
 const credentials = require('../credentials.json');
 
-const provider = new ethers.providers.JsonRpcProvider(credentials.private_key);
-const signer = new ethers.Wallet(credentials.private_key, provider);
-
-// Deployed card: 0x0165878A594ca255338adfa4d48449f69242Eb8F
-// Deployed snapshot: 0x0165878A594ca255338adfa4d48449f69242Eb8F
-// Deployed pack: 0x0165878A594ca255338adfa4d48449f69242Eb8F
-
 async function main() {
   console.log('start');
+
+  const provider = new ethers.providers.JsonRpcProvider(credentials.rpc);
+  await provider.ready;
+  const signer = new ethers.Wallet(credentials.private_key, provider);
+
+  // Deployed card: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+  // Deployed snapshot: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+  // Deployed pack: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+
+  const abi = require('../abi/Snapshot.json');
+  const address = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
+  const snapshot = new ethers.Contract(address, abi, signer);
+
   const data = fs.readFileSync('../snapshot/snapshot.csv', 'utf8');
   const owners = data.split('\n');
   let allOwners = [];
@@ -23,10 +29,7 @@ async function main() {
     allCards.push(cardId);
     allOwners.push(ownerAddress);
 
-    if (allCards.length == 20) {
-      const abi = require('../abi/Snapshot.json');
-      const address = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
-      const snapshot = new ethers.Contract(address, abi, signer);
+    if (allCards.length == 3) {
       await snapshot.setOwners(allOwners, allCards);
 
       console.log(allCards[0]);
