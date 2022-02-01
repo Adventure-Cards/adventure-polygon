937d643fdb9dc0faddb1fa1a0614d256ea20f8d4
Paul McKellar <git@paulmckellar.com>
2022-02-01 08:22:24 -0500

    set snap

---
+++ polygon-contracts/scripts/setSnapshot.js
@@ -0,0 +1,52 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const hre = require('hardhat');
+const fs = require('fs');
+
+const credentials = require('../credentials.json');
+
+const provider = new ethers.providers.JsonRpcProvider(credentials.private_key);
+const signer = new ethers.Wallet(credentials.private_key, provider);
+
+// Deployed card: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+// Deployed snapshot: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+// Deployed pack: 0x0165878A594ca255338adfa4d48449f69242Eb8F
+
+async function main() {
+  console.log('start');
+  const data = fs.readFileSync('../snapshot/snapshot.csv', 'utf8');
+  const owners = data.split('\n');
+  let allOwners = [];
+  let allCards = [];
+
+  owners.forEach(async (line) => {
+    const [cardId, ownerAddress] = line.split(';');
+    allCards.push(cardId);
+    allOwners.push(ownerAddress);
+
+    if (allCards.length == 20) {
+      const abi = require('../abi/Snapshot.json');
+      const address = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
+      const snapshot = new ethers.Contract(address, abi, signer);
+      await snapshot.setOwners(allOwners, allCards);
+
+      console.log(allCards[0]);
+      allOwners = [];
+      allCards = [];
+    }
+  });
+  console.log('done');
+
+  // for (let i = 0; i < allOwners.length / 10; i++) {
+  //   const a = allCards.pop();
+  //   console.log('file: deploy.js ~ line 49 ~ main ~ a', a);
+  // }
+  // console.log('feezing!');
+  // await snapshot.freeze();
+  // console.log('minting!');
+  // await snapshot.mint(1, 3);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
